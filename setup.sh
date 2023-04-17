e1418291b3a56a9871336d4d48c60ab206feb478
polyacetal <49859774+polyacetal@users.noreply.github.com>
2023-04-17 21:03:18 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#apt
+yes | sudo apt update
+yes | sudo apt upgrade
+
+#lang
+yes | sudo apt install gauche
+yes | sudo apt install python3
+yes | sudo apt install nodejs
+yes | sudo apt install npm
+
+#Editor
+yes | sudo apt install vim emacs
+
+#SetupEditor
+wget https://raw.githubusercontent.com/KNCT-KPC/dotfiles/master/.vimrc
+
+#Tool
+yes | sudo apt install tree
+yes | sudo apt install curl
+yes | sudo apt install git
+yes | sudo apt install tmux
+yes | sudo apt install docker
+yes | sudo apt install virtualbox
+yes | sudo apt install vagrant
+
+#Joke
+yes | sudo apt install sl
+yes | sudo apt install cowsay
+yes | sudo apt install screenfetch
