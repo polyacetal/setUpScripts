607b1cc45b0365c4047cf73e66602e4519281d2c
polyacetal <49859774+polyacetal@users.noreply.github.com>
2023-04-18 11:27:17 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,3 +26,16 @@ yes | sudo apt install vagrant
 yes | sudo apt install sl
 yes | sudo apt install cowsay
 yes | sudo apt install screenfetch
+
+#VSCode
+#https://inno-tech-life.com/dev/linux/install_vscode/ 参照
+# GPGキーをダウンロード(配布先のものが改ざん等がないかを確認するための公開鍵) 
+# apt-key add にて信頼できるキーとして登録
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+# microsoftのリポジトリをaptリポジトリに追加
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+# リポジトリの更新 & インストール
+sudo apt update
+sudo apt install -y code 
