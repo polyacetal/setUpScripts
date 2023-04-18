bb55ca95adc7deecbcb8d04780475ebc44a7dd8f
polyacetal <49859774+polyacetal@users.noreply.github.com>
2023-04-18 11:28:32 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,5 +37,5 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 
 # リポジトリの更新 & インストール
-sudo apt update
-sudo apt install -y code 
+yes | sudo apt update
+yes | sudo apt install code 
